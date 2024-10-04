883449af4b19e8c061fdbe868c41cf912130b32f
devjoaogf <144185914+devjoaogf@users.noreply.github.com>
2024-10-03 20:41:46 -0400

    Update page.tsx

--- code_area/loja-de-roupas/src/app/page.tsx
+++ code_area/loja-de-roupas/src/app/page.tsx
@@ -1,8 +1,10 @@
-
 //import Sobre from "@/pages/Sobre";
+//import Login from "@/pages/Login";
+//import Register from "@/pages/Register";
+//import Cadastro from "@/pages/Cadastro";
 import Produtos from "@/pages/Produtos";
 
-
+//Só carrega uma página por vez, temporário
 
 
 export default function Home() {
