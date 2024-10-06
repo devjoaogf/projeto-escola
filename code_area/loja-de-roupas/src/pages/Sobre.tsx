05ed6d8714ea9dddf8e39a6ec7f132e1e9488203
devjoaogf <144185914+devjoaogf@users.noreply.github.com>
2024-10-06 13:12:54 -0400

    Update Sobre.tsx

--- code_area/loja-de-roupas/src/pages/Sobre.tsx
+++ code_area/loja-de-roupas/src/pages/Sobre.tsx
@@ -30,7 +30,7 @@ export default function Sobre(){
                         </div>
                     </div>
                     <div className="flex flex-col lg:w-1/2">
-                           <img className="border-5 border-imagem-500" src={Fundo.src} alt="Vestido Ladie" />
+                           <img className="border-8 border-imagem-500" src={Fundo.src} alt="Vestido Ladie" />
                             </div>
 
                     </div>
