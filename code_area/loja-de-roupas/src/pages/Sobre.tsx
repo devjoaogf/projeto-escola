740f90cb48649bff30524c6fc43518b4904d4078
devjoaogf <144185914+devjoaogf@users.noreply.github.com>
2024-10-06 13:15:49 -0400

    Add files via upload

--- code_area/loja-de-roupas/src/pages/Sobre.tsx
+++ code_area/loja-de-roupas/src/pages/Sobre.tsx
@@ -1,18 +1,17 @@
 /* eslint-disable */
-
 import AlternativeButton from "@/components/Buttons/AlternativeButton";
 import PrimaryButton from "@/components/Buttons/PrimaryButton";
 import Menu from "@/components/Menu";
-import Fundo from"@/assets/Vestido Melissa.jpg"
+import Fundo from "@/assets/Vestido Melissa.jpg"
 
 
 // Cada classname altera as props do menu principal
-export default function Sobre(){
+export default function Sobre() {
 
-    return(
+    return (
         <div>
-            <Menu/>
-            
+            <Menu />
+
             <div className="bg-gradient">
                 <div className="flex flex-col flex-wrap items-center p-5 lg:px-40 lg:flex-row">
                     <div className="flex flex-col px-4 text-center mb-5 lg:w-1/2 gap-4 lg:text-left">
@@ -21,22 +20,20 @@ export default function Sobre(){
                         <p className="texto-sobre font-bold lg:text-2xl">Produtos importados para você se vestir com elegância.</p>
                         <div className="flex flex-col gap-4 lg:flex-row">
                             <PrimaryButton
-                            text="Ir para a loja"
+                                text="Ir para a loja"
                             />
 
                             <AlternativeButton
-                            text="Ver carrinho"
+                                text="Ver carrinho"
                             />
                         </div>
                     </div>
                     <div className="flex flex-col lg:w-1/2">
-                           <img className="border-8 border-imagem-500" src={Fundo.src} alt="Vestido Ladie" />
-                            </div>
-
+                        <img className="border-8 border-imagem-500" src={Fundo.src} alt="Vestido Ladie" />
                     </div>
                 </div>
             </div>
-        
+        </div>
     );
 }
 
